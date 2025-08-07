9852b74bd6b2cd75161e479e871d92db43bb1e14
AleeMusic <dream.alex@tiscali.it>
2025-08-07 10:09:05 +0200

    Delete NOMEMUSIC directory

--- NOMEMUSIC/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  if (!localStorage.getItem("cookiesAccepted")) {
-    document.getElementById("cookie-banner").style.display = "block";
-  }
-  document.getElementById("accept-cookies").addEventListener("click", () => {
-    localStorage.setItem("cookiesAccepted", true);
-    document.getElementById("cookie-banner").style.display = "none";
-  });
-});
